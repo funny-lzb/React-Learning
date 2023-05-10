a949c750d31580cf1fedae3e351fb1712cc79773
unknown <lzb129239@gmail.com>
2023-05-10 21:31:31 +0800

    useLocalStorage project once by myself

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,28 +1,27 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 /* TodoList:
-    
-   
+
+
 */
 export function useLocalStorage(key, initialValue) {
-  // 2.组件初次渲染时，表单的value是从本地存储中拿出来的.
+  // 2.当你初次渲染页面时，你表单的value是由上一次本地存储决定的
+  // 如果上一次本地存储的值存在，我们把它作为表单的value
+  // 如果上一次本地存储的值不存在，我们就用初始值作为表单的value
   const [value, setValue] = useState(() => {
     const localValue = localStorage.getItem(key)
     if (localValue == null) {
-      if (typeof initialValue === 'function') {
-        return initialValue()
-      } else {
-        return initialValue
-      }
+      if (typeof initialValue === 'function') return initialValue()
+
+      return initialValue
     } else {
       return JSON.parse(localValue)
     }
   })
 
-  // 1.每当我value改变时，我都要把它放到本地存储里
+  //   1.每当你改变value时，都要把它同步进本地存储中
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value))
-  }, [value])
-
+  }, [value, key])
   return [value, setValue]
 }
